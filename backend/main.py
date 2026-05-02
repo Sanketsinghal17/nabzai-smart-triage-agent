0da12653d530fdd87338ce252f0e77a06ebaa4ea
Sanket Singhal <sanketsinghal6@gmail.com>
2026-05-02 10:41:29 +0530

    Fixed result page for backend response structure

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,15 @@ from pydantic import BaseModel
 from decision_engine import analyze_case
 from scheduler import get_doctor_and_slot
 from planner import get_steps
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class PatientInput(BaseModel):
     symptoms: list
